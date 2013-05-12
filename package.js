ca1ae1492281dc1a804ee77f48a85619145fb7d3
Gadi Cohen <dragon@wastelands.net>
2013-05-12 13:32:11 +0300

    added missing refs to jquery.Jcrop.css and Jcrop.gif

--- package.js
+++ package.js
@@ -4,5 +4,9 @@ Package.describe({
 
 Package.on_use(function (api) {
 	api.use('jquery', 'client');
-	api.add_files(['lib/Jcrop/js/jquery.Jcrop.js'],'client');
+	api.add_files([
+		'lib/Jcrop/js/jquery.Jcrop.js',
+		'lib/Jcrop/css/jquery.Jcrop.css',
+		'lib/Jcrop/css/Jcrop.gif'
+	], 'client');
 });
