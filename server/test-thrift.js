1edfac080b406e2335497334bd36ef6c9ca4bbd4
Zephyre <haizi.zh@gmail.com>
2015-10-19 17:59:03 +0800

    FIX: hotfix

--- server/test-thrift.js
+++ server/test-thrift.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
-  var service = Npm.require('smscenter');
-  var SmsCenter = service.SmsCenter;
+  //var service = Npm.require('smscenter');
+  //var SmsCenter = service.SmsCenter;
 
-  var client = ThriftHelper.createClient(SmsCenter, 'localhost', 9090, ['_ping', 'sendSms'], {
-    transport:'framed'
-  });
-  console.log(client._ping());
+  //var client = ThriftHelper.createClient(SmsCenter, 'localhost', 9090, ['_ping', 'sendSms'], {
+  //  transport:'framed'
+  //});
+  //console.log(client._ping());
 });
