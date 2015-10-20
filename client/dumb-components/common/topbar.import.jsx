f91f1366f8b27f9cbacbf3a0524e3a0bf4117bfb
lyn <buaaxigua@gmail.com>
2015-10-20 21:01:15 +0800

    ADD navigator & add the minimalize

--- client/dumb-components/common/topbar.import.jsx
+++ client/dumb-components/common/topbar.import.jsx
@@ -1,4 +1,35 @@
 let topBar = React.createClass({
+  componentDidMount() {
+    // Toggle left navigation
+    $('#navbar-minimalize').on('click', function (event) {
+      console.log('click');
+      console.log(event);
+      event.preventDefault();
+
+      // Toggle special class
+      $("body").toggleClass("mini-navbar");
+
+      // Enable smoothly hide/show menu
+      if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+        // Hide menu in order to smoothly turn on when maximize menu
+        $('#side-menu').hide();
+        // For smoothly turn on menu
+        setTimeout(
+          function () {
+            $('#side-menu').fadeIn(500);
+          }, 100);
+      } else if ($('body').hasClass('fixed-sidebar')) {
+        $('#side-menu').hide();
+        setTimeout(
+          function () {
+            $('#side-menu').fadeIn(500);
+          }, 300);
+      } else {
+        // Remove all inline style from jquery fadeIn function to reset menu state
+        $('#side-menu').removeAttr('style');
+      }
+    });
+  },
   render() {
     return (
       <nav className="navbar navbar-static-top" role="navigation" style={{marginBottom: 0}}>
