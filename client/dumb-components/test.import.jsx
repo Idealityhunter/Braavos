dd24a325f1a682b16402e0adbae66d48868b3a2b
Zephyre <haizi.zh@gmail.com>
2015-10-20 14:08:08 +0800

    MOD: 添加国际化/本地化支持

--- client/dumb-components/test.import.jsx
+++ client/dumb-components/test.import.jsx
@@ -1,9 +1,53 @@
-let test = React.createClass({
-  render() {
+var IntlMixin = ReactIntl.IntlMixin;
+var FormattedNumber = ReactIntl.FormattedNumber;
+var FormattedMessage = ReactIntl.FormattedMessage;
+var FormattedRelative = ReactIntl.FormattedRelative;
+
+var PostMeta = React.createClass({
+  mixins: [IntlMixin],
+
+  render: function () {
     return (
-      <div>Hello World</div>
+      <FormattedMessage
+        message={this.getIntlMessage('post.meta')}
+        num={<FormattedNumber value={this.props.post.comments.length + 2389} />}
+        ago={<FormattedRelative value={this.props.post.date} />}/>
     );
   }
 });
 
+var post = {
+  date: 1422046290531,
+  comments: ['asf', 'af']
+};
+
+//var intlData = {
+//  locales: ['en-US'],
+//  messages: {
+//    post: {
+//      meta: 'Posted {ago}, {num} comments'
+//    }
+//  }
+//};
+
+let test = React.createClass({
+  mixins: [IntlMixin],
+
+  render() {
+    return (
+      <div>
+        <p>
+          <FormattedMessage message={this.getIntlMessage('welcome')} />
+        </p>
+
+        <p>
+          <FormattedNumber value={4200}/>
+        </p>
+      </div>
+
+    );
+  }
+});
+
+//<FormattedNumber locales={['en-US']} local='en-US' value={1000} style="currency" currency="USD" />
 export const Test = test;
---
+++ client/i18n/resource/zh.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Created by zephyre on 10/20/15.
+ */
+
+AppDeps.IntlData = AppDeps.IntlData || {};
+
+AppDeps.IntlData.zh = {
+  locales: ['zh-CN'],
+  messages: {
+    welcome: "欢迎来到Braavos",
+    post: {
+      meta: '发表于{ago}, 一共有{num}条评论.'
+    }
+  }
+};
