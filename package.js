7ef518f87be7940181e800f8b09bc405dc480b70
Zephyre <haizi.zh@gmail.com>
2015-11-05 18:27:56 +0800

    MOD: use jquery.Jcrop.min.js instead

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 Package.on_use(function (api) {
 	api.use('jquery', 'client');
 	api.add_files([
-		'lib/Jcrop/js/jquery.Jcrop.js',
+		'lib/Jcrop/js/jquery.Jcrop.min.js',
 		'lib/Jcrop/css/jquery.Jcrop.css',
 		'lib/Jcrop/css/Jcrop.gif'
 	], 'client');
