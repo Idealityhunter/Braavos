3103a24993c9e81bf33874e82b629b847d3ae745
lyn <buaaxigua@gmail.com>
2015-11-13 14:46:48 +0800

    FIX: 修改添加商品中的文本框的placeholder和defaultvalue

--- client/dumb-components/commodity/commodityModifyBook.import.jsx
+++ client/dumb-components/commodity/commodityModifyBook.import.jsx
@@ -3,27 +3,33 @@ var FormattedMessage = ReactIntl.FormattedMessage;
 
 let commodityModifyBook = React.createClass({
   mixins: [IntlMixin],
-  componentWillMount() {
-    // TODO
+  styles: {
+    asterisk: {
+      color: 'coral',
+      verticalAlign: 'text-top',
+      paddingLeft: 5,
+      fontsize: 18
+    }
   },
-  componentDidMount() {
-    $('textarea').on('blur', function(e){
-      console.log(e.target.value);
-    });
+
+  placeholders: {
+    book: '请输入预定流程',
+    unbook: '请输入退订或改订的相关规定'
   },
+
   render() {
     return (
       <div className="commodity-book-wrap">
         <form className="form-horizontal">
           <div className="form-group book">
-            <label className="">预定流程*</label>
-            <textarea className="form-control" rows="3" placeholder="请输入预定流程"/>
+            <label className="">预定流程<span style={this.styles.asterisk}>*</span></label>
+            <textarea className="form-control placeholder" rows="3" placeholder={this.placeholders.book}/>
           </div>
         </form>
         <form className="form-horizontal">
           <div className="form-group unbook">
-            <label className="">退改规定*</label>
-            <textarea className="form-control" rows="3" placeholder="请输入退订或改订的相关规定"/>
+            <label className="">退改规定<span style={this.styles.asterisk}>*</span></label>
+            <textarea className="form-control placeholder" rows="3" placeholder={this.placeholders.unbook}/>
           </div>
         </form>
       </div>
