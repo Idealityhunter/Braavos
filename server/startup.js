bbc26824aadc41aafe5ea3884045aef183517ce8
Zephyre <haizi.zh@gmail.com>
2015-12-02 13:03:59 +0800

    MOD: 修改环境设置

--- server/startup.js
+++ server/startup.js
@@ -11,14 +11,16 @@ function resolveEtcdData() {
   const url = `${process.env['ETCD_HOST'] || "localhost"}:${process.env['ETCD_PORT'] || 2379}`;
 
   const services = new EtcdHelper.EtcdServiceBuilder(url)
-    .addEntry(['mongo-dev', 'mongo'])
-    .addEntry(['yunkai-dev', 'yunkai'])
+    .addEntry(['mongo-production', 'mongo'])
+    .addEntry('yunkai')
     .addEntry('smscenter')
     .addEntry('idgen')
     .addEntry(['hedy-base', 'hedy'])
-    .addEntry(['hedy-dev', 'hedy'])
+    .addEntry('hedy')
     .build();
-  const config = new EtcdHelper.EtcdConfigBuilder(url).addEntry('braavos').build();
+  const config = new EtcdHelper.EtcdConfigBuilder(url)
+    .addEntry(['braavos-base', 'braavos'])
+    .addEntry(['braavos-production', 'braavos']).build();
   BraavosCore.RootConf = _.extend(services, config);
 }
 
