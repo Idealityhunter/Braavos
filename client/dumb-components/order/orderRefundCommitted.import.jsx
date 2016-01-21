0d96a5ce89eff9e6328288b5d2b623ee7443e4a0
lyn <buaaxigua@gmail.com>
2016-01-21 17:15:02 +0800

    FIX: 暂时将退款处理时限全部设置为72小时(发货/未发货)

--- client/dumb-components/order/orderRefundCommitted.import.jsx
+++ client/dumb-components/order/orderRefundCommitted.import.jsx
@@ -232,7 +232,7 @@ const orderRefundCommitted = React.createClass({
           <div className="ibox-content" style={{padding: 30}}>
             <div>
               <h3 className="inline">请处理退款</h3>
-              <span style={this.styles.countDown}>倒计时: {this._getCountDown('refundApply', 96)}</span>
+              <span style={this.styles.countDown}>倒计时: {this._getCountDown('refundApply', 72)}</span>
             </div>
 
             <ol style={this.styles.ol}>
