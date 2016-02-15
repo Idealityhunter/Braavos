4b8fc76e2b8fc21835395efc5150a96f70ce60a7
Zephyre <haizi.zh@gmail.com>
2016-02-15 16:02:28 +0800

    MOD: 处理message不合法的情况

--- client/dumb-components/message/systemMessage/systemMessage.import.jsx
+++ client/dumb-components/message/systemMessage/systemMessage.import.jsx
@@ -11,7 +11,7 @@ export const SystemMessage = React.createClass({
     FlowRouter.go(`/orders/${JSON.parse(this.props.msg.contents).orderId}`);
   },
 
-  styles:{
+  styles: {
     container: {
       borderRight: '1px solid #ccc',
       display: 'inline-block',
@@ -31,17 +31,31 @@ export const SystemMessage = React.createClass({
     }
   },
   render(){
-    const contents = JSON.parse(this.props.msg.contents);
-    return (
-      <div style={this.styles.container} onClick={this.showOrderInfo}>
-        <div>
-          <h3>{contents.title}</h3>
-          <p style={this.styles.text}>{contents.text}</p>
-          <hr style={this.styles.hr}/>
-          <p style={this.styles.text}>商品名称: {contents.commodityName}</p>
-          <p style={this.styles.text}>订单编号: {contents.orderId}</p>
+    const contents = (() => {
+      try {
+        return JSON.parse(this.props.msg.contents);
+      } catch (err) {
+        BraavosCore.logger.debug(`Unable to parse the message body: ${this.props.msg.contents}`);
+        return undefined;
+      }
+    })();
+
+    if (contents) {
+      return (
+        <div style={this.styles.container} onClick={this.showOrderInfo}>
+          <div>
+            <h3>{contents.title}</h3>
+            <p style={this.styles.text}>{contents.text}</p>
+            <hr style={this.styles.hr}/>
+            <p style={this.styles.text}>商品名称: {contents.commodityName}</p>
+            <p style={this.styles.text}>订单编号: {contents.orderId}</p>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div />
+      );
+    }
   }
 });
